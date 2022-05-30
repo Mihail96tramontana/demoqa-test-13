1235908a61b8c44eef7795f650fe9aa1d73342a1
parnishca <xelysqgbhju@gmail.com>
2022-05-30 09:02:03 +0300

    added .gitignore

---
+++ src/test/java/tests/TextBoxTest.java
@@ -0,0 +1,33 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void successfulTest() {
+        open("/text-box");
+
+        $("[id=userName]").setValue("Mihail");
+        $("[id=userEmail]").setValue("Mihail@mail.ru");
+        $("[id=currentAddress]").setValue("Some address 1");
+        $("[id=permanentAddress]").setValue("Another address 2");
+        $("[id=submit]").click();
+
+        $("[id=output]").shouldHave(text("Mihail"), text("Mihail@mail.ru"),
+                text("Some address 1"), text("Another address 2"));
+    }
+}
